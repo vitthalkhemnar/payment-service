67b8377e8df4fa10b48527eaa7a55407e5c37208
Vitthal Khemnar <vitthalkkhemnar@gmail.com>
2026-09-25 04:53:55 +0530

    implemented api gateway

--- src/main/java/com/ecom/payment/PaymentServiceApplication.java
+++ src/main/java/com/ecom/payment/PaymentServiceApplication.java
@@ -2,8 +2,9 @@ package com.ecom.payment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.security.autoconfigure.UserDetailsServiceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = UserDetailsServiceAutoConfiguration.class)
 public class PaymentServiceApplication {
 
 	public static void main(String[] args) {
